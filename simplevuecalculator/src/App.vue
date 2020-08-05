d40211ce841667b084c6cde71ed21ec73176e0ba
SuperiorRev <amandhiraj@live.ca>
2020-08-05 01:14:19 -0400

    Initial update

---
+++ simplevuecalculator/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <h1>Simple Vue Calculator</h1>
+    <p>By: Aman Dhiraj</p>
+    <br>
+    <Calculator></Calculator>
+  </div>
+</template>
+
+<script>
+import Calculator from "./components/Calculator.vue";
+
+export default {
+  name: "app",
+  components: {
+    Calculator,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Courier New", Courier, monospace;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #e74c3c;
+  margin-top: 60px;
+}
+
+h1 {
+  font-size: 3em;
+}
+
+img {
+  max-width: 50px;
+  max-height: 80px;
+  height: 80px;
+}
+</style>
